5344e0c3397ab913288a59585dbb0631121305f0
Ferhat Demir <153025655+FerhatDemr@users.noreply.github.com>
2023-12-29 18:50:04 +0300

    Add files via upload

---
+++ Determinant_hesaplama.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+main()
+{
+	// Kullanıcıdan 4 tane sayı girdisi alıp bu sayıları matris içine yerleştiren ve bu matrisin determinantını hesaplayan program.
+	int i,j;
+	int a,b,c,d;
+	int determinant;
+	int matris[2][2];
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+		
+			scanf("%d",&matris[i][j]);
+			
+			
+		}
+		
+
+
+		
+		
+	}
+
+	for(i=0;i<2;i++)
+	{
+		printf("|");
+		for(j=0;j<2;j++)
+		{
+		
+			printf("%d",matris[i][j]);
+		}
+		printf("|\n");
+	}
+	
+	a=matris[0][0];
+	b=matris[0][1];
+	c=matris[1][0];
+	d=matris[1][1];
+	
+	determinant= (a*d )- (b * c);
+	
+	
+	
+	printf("%d",determinant);
+	
+	
+	
+	
+	
+}
